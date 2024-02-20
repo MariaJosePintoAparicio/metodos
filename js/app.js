4dde231d179fcb8e3c3d67f9a9699980ec313235
MariaJosePintoAparicio <152021983+MariaJosePintoAparicio@users.noreply.github.com>
2024-02-20 08:51:48 -0500

    HOLA

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+//*const array1 = [1, 2 , 3 ];
+//*const array2 = [4, 5 , 6 ];
+
+//*const array3 = [...array1, ...array2];
+//*console.log(array3)
+/**me da este resultado el ejercicio (6) [1, 2, 3, 4, 5, 6]**/
+
+
+//*const original = [1 , 2 , 3 ];
+//*const clone = [...original];
+//*console.log(clone);
+/**  (3) [1, 2, 3] esto es lo que me da */
+
+
+//*function suma(a , b , c) {
+   //* return a + b + c;
+//*}
+//*const numbers = [1, 2 , 3];
+//*const result = sum(...numbers);
+//*console.log(result);
+/**esto nos da 6 */
+
+
+//*const cambiaNombre = (persona) => {
+   //* persona.nombre = 'tony' ;
+    //*return persona;
+//*}
+//*let peter = {nombre : 'peter'};
+//*let tony = cambiaNombre (peter);
+
+//*console.log({peter , tony})
+/**este es el resultado {peter: {…}, tony: {…}} */
+
+// Función flecha
+//*(a, b) => {
+   //* let chuck = 42;
+    //*return a + b + chuck;
+  //*}
+
+// Función flecha
+//*var add = (a, b, c) => this.num + a + b + c;
